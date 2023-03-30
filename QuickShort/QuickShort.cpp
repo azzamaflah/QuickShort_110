af2db7935dacde9fa307912bd8f87d125967e923
azzamaflah <azzamhebat46@gmail.com>
2023-03-30 16:41:26 +0700

    mencari elemen yang kurang dari atau sama dengan pivot

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -68,7 +68,7 @@ void q_short(int low, int high)
 		cmp_count++;
 
 		//search for an element less than or equal to pivot
-		while ((arr[j] > pivot) && (j >= low))		//Langkah Algoritma No.7
+		while ((arr[j] > pivot) && (j >= low)) 		//Langkah Algoritma No.7
 		{
 			j--;									//Langkah Algoritma No.8
 			cmp_count++;

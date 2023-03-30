59ff150108c8c1be246270aef6d678a6038d3446
azzamaflah <azzamhebat46@gmail.com>
2023-03-30 16:33:01 +0700

    input comparasi

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -5,7 +5,7 @@
 using namespace std; 
 
 int arr[20];				//array of integers to hold velues
-int cmp_count = 0;			//number of comparasion
+int cmp_count = 0;		 	//number of comparasion
 int mov_count = 0;
 int n;
 

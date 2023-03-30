5f452095138d8d0fbcd267eb388be2b6f87d88d4
azzamaflah <azzamhebat46@gmail.com>
2023-03-30 16:33:48 +0700

    input index x dengan elemet index y

--- QuickShort/QuickShort.cpp
+++ QuickShort/QuickShort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int arr[20];				//array of integers to hold velues
 int cmp_count = 0;		 	//number of comparasion
-int mov_count = 0;
+int mov_count = 0; 
 int n;
 
 void input() {
